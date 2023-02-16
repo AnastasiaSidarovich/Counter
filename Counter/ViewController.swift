429fe37d57bcefb2d2105f6c1a095caf6cce7d69
Anastasia Sidarovich <mc.sidr@yandex.by>
2023-02-16 23:09:00 +0300

    first commit

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var clickButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    var counter: Int = 0 {
+        didSet {
+           updateCounterLabel()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counter = 0
     }
 
-
+    @IBAction func buttonDidTouch(_ sender: Any) {
+        counter += 1
+    }
+    
+    private func updateCounterLabel() {
+        counterLabel.text = "Значение счётчика:\n \(counter)"
+    }
 }
 
